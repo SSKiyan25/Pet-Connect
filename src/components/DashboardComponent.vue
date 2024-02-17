52db3819c6d2420b05bd91f31237f852962a642a
Josh <126632428+SSKiyan25@users.noreply.github.com>
2024-02-18 02:56:00 +0800

    Update DashboardComponent.vue

--- src/components/DashboardComponent.vue
+++ src/components/DashboardComponent.vue
@@ -1,18 +1,10 @@
 <template>
     <div class="flex flex-row justify-between px-24 space-x-8 w-full h-fit mt-28">
         <div class="flex-auto w-32 h-48 rounded sm:w-96 dark:bg-gray-700">
-            <p>
-                <span class="text-2xl font-semibold text-gray-800 dark:text-gray-200">Welcome to PetConnect</span>
-            </p>
-            <p class="mt-4 text-gray-600 dark:text-gray-300">
+            <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-5xl dark:text-white">Welcome to PetConnect!</h1>
+            <p class="mt-6 ml-2 mr-12 text-justify text-sm text-gray-600 dark:text-gray-300">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae efficitur turpis, vitae consectetur arcu. Integer sed ipsum non lacus tempus blandit sed in nibh. Suspendisse pharetra scelerisque laoreet. Donec ac aliquet urna. Proin non felis suscipit, gravida tellus vel, iaculis sapien. Ut eu cursus justo. Donec vulputate viverra urna, in pellentesque augue hendrerit vel. Here's how to adopt a pet:
             </p>
-            <div class="flex flex-row mt-5">
-                <img src="/pet-connect-logo.png" class="h-8 w-auto mt-3 ml-8">
-                <p class="mt-5 ml-2 text-gray-600 dark:text-gray-300">
-                Submit the adoption application form
-                </p>
-            </div>
             <div class="flex flex-row mt-3">
                 <img src="/pet-connect-logo.png" class="h-8 w-auto mt-3 ml-8">
                 <p class="mt-5 ml-2 text-gray-600 dark:text-gray-300">
@@ -24,6 +16,20 @@
                 <p class="mt-5 ml-2 text-gray-600 dark:text-gray-300">
                 Submit the adoption application form
                 </p>
+            </div>
+            <div class="flex flex-row mt-3">
+                <img src="/pet-connect-logo.png" class="h-8 w-auto mt-3 ml-8">
+                <p class="mt-5 ml-2 text-gray-600 dark:text-gray-300">
+                Submit the adoption application form
+                </p>
+            </div>
+            <div class="flex flex-row mt-6 justify-center">
+                <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-[#6892D5] rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Learn more
+                    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </a>
             </div>
         </div>
         <div class="flex-auto w-64 items-center md-flex-row h-48 rounded sm:w-96">
