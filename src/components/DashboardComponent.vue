1a8c29d08ed365903d6ff76de527d91ae4fd418c
Josh <126632428+SSKiyan25@users.noreply.github.com>
2024-02-18 00:18:20 +0800

    Update DashboardComponent.vue

--- src/components/DashboardComponent.vue
+++ src/components/DashboardComponent.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex flex-col items-center justify-center h-screen">
-        <div class="flex flex-col md:flex-row md:space-x-8">
-            <div class="flex-1 items-center justify-center w-full h-48 rounded sm:w-96 dark:bg-gray-700">
+    <div class="flex flex-col justify-center h-fit mt-28">
+        <div class="flex md:flex-row md:space-x-8">
+            <div class="flex-auto w-32 h-48 rounded sm:w-96 dark:bg-gray-700">
                 <p>
                     <span class="text-2xl font-semibold text-gray-800 dark:text-gray-200">Welcome to PetConnect</span>
                 </p>
@@ -9,12 +9,12 @@
                     asdas
                 </p>
             </div>
-            <div class="contents">
-                <div class="flex-1 items-center justify-center w-full h-48 rounded sm:w-96">
-                    <img src="/cat-1.jpg" alt="Featured Image" />
-                </div>
+            <div class="flex-auto w-64 items-center justify-center h-48 rounded sm:w-96">
+                <img src="/cat-1.jpg" alt="Featured Image" />
             </div>
         </div>
-        
+    </div>
+    <div class="flex flex-col h-screen">
+
     </div>
 </template>
